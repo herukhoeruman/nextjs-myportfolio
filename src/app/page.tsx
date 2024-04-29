43b5c06d537dc95bf36fbf2f425449dee033bd58
herukhoeruman <32447416+herukhoeruman@users.noreply.github.com>
2024-04-29 09:55:45 +0700

    adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col gap-[36px] pb-8">
-      <h1 className="font-bold text-[48px]">Hi!</h1>
+      <h1 className="font-bold text-[48px]">Hi, I am Heru!</h1>
       <p>
-        I'm Heru, a <span className="font-bold">Software Engineer at</span>
+        A <span className="font-bold">Software Engineer at</span>
         <Link
           href={"https://gsp.co.id"}
           className="underline underline-offset-2 font-bold"
@@ -23,6 +23,7 @@ export default function Home() {
         applications using modern technologies, such as React, Next.js, Svelte,
         Node.js, Express.js, Hono.js and TypeScript.
       </p>
+      <p className="font-bold">Web ini masih dalam pengembangan ya gess!!!</p>
       <div className="inline-flex gap-[11px] underline underline-offset-2">
         <Link href={"https://www.github.com"}>Github</Link>
         <Link href={"https://www.linkedin.com"}>LinkedIn</Link>
